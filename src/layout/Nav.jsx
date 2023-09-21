c04825fd9c33b54c982c10c2d9ec2a1c143e95a5
민규 <xiahtic73@gmail.com>
2023-09-21 11:46:48 +0900

    modify : css 수정

--- src/layout/Nav.jsx
+++ src/layout/Nav.jsx
@@ -3,23 +3,23 @@ import { NavLink } from "react-router-dom";
 const NAV_MENU = [
   { title: "피드", url: "feed" },
   { title: "리뷰", url: "my-review" },
-  { title: "방문/예약", url: "visit" },
+  { title: "방문/예약", url: "reservation" },
   { title: "저장", url: "favorite" },
 ];
 
 function Nav() {
   return (
-    <nav className="sticky top-0 z-10 border-gray-800 bg-gray-50 shadow-md mb-2">
-      <ul className="mx-auto flex max-w-2xl items-center justify-around text-lg">
+    <nav className="sticky top-0 z-10 mb-2 border-gray-800 bg-gray-50 shadow-md">
+      <ul className="border-box mx-auto flex max-w-2xl items-center justify-around pt-3 text-lg">
         {NAV_MENU.map((item) => {
           return (
             <li key={crypto.randomUUID()}>
               <NavLink
                 to={`/${item.url}`}
                 className={({ isActive }) => {
-                  const baseClassName = "px-3 py-1 pt-3";
+                  const baseClassName = "px-3";
                   return isActive
-                    ? `${baseClassName} box-border block border-b-4 border-primary font-extrabold text-primary`
+                    ? `${baseClassName} block border-b-4 border-primary font-extrabold text-primary`
                     : `${baseClassName} text-secondary`;
                 }}
               >
--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -3,13 +3,12 @@ import Footer from "./Footer";
 import Nav from "./Nav";
 import Header from "./header";
 import { Outlet } from "react-router-dom";
-import {divide} from 'ramda';
 
 function RootLayout() {
   return (
     <div>
       <Header />
-      <div className="bg-gray-50 relative">
+      <div className="bg-gray-50">
         <Profile />
       </div>
       <Nav />
